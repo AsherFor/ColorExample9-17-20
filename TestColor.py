0d045da49b8e6239972f2e15556bd014aaada4a5
AsherFor <asherforman@stu.postoakschool.org>
2020-09-17 09:59:52 -0700

    Final Final

--- TestColor.py
+++ TestColor.py
@@ -14,13 +14,18 @@ print ('%s Hello World !!! %s' % (fg(1), attr(0)))
 print ('%s%s Hello World !!! %s' % (fg(1), bg(15), attr(0)))
 print ('%s%s Hello World !!! %s' % (fg('white'), bg('yellow'), attr('reset')))
 
-print (color + "|\---/|" + res)
-print (color + "| o_o |" + res)
-print (color + " \_^_/" + res)
+# print (color + "|\---/|" + res)
+# print (color + "| o_o |" + res)
+# print (color + " \_^_/" + res)
 
 print (color + "      |\      _,,,---,,_" + res)
 print (color + "ZZZzz /,`.-'`'    -.  ;-;;,_" + res)
 print (color + "     |,4-  ) )-,_. ,\ (  `'-'" + res)
 print (color + "    '---''(_/--'  `-'\_)" + res)
 
-
+for num in range(16):
+    # color = fg('#C0C0C0') + bg('#00005f')
+    str_num = str(num)
+    hex = ('#' + str_num + str_num + str_num)
+    color = fg(hex) + bg('#00005f')
+    print(color + "| o_o |" + res)
